dedadf3d12c63cc9c3173e89ca356f5d48fdad52
Ryan Ernst <rernst@Ryans-Mac-mini.local>
2025-01-11 12:33:50 -0500

    Cleaned up with thread-last macros

--- day1.clj
+++ day1.clj
@@ -3,13 +3,20 @@
 (defn get-input [path] 
   ;; Returns the innput as a list of two lists where each
   ;; list correspondes to one column of the input
-  (map (fn [pair] (map #(Integer/parseInt %) pair))
-  (map #(str/split % #"\s+") (str/split-lines (slurp path))))
-)
+  (->> (slurp path) ;; Use threading macro to chain the operations
+       str/split-lines ;; Start by splitting on lines in slurped file
+       (map #(str/split % #"\s+")) ;; map split function and split on whitespace
+       (map (fn [pair] (map #(Integer/parseInt %) pair))))) ;; Parse as int, use anonymous function to handle pairs 
 
-(let [input (get-input "day1part1.input")]
-  (apply + (map abs (apply map - (map sort (apply map list input)) )) )
-) ; 2430334
+( defn solve-p1 [path] 
+  (->> (get-input path)
+       (apply map list)
+       (map sort)
+       (apply map -)
+       (map abs)
+       (reduce +)))
+
+(solve-p1 "day1part1.input") ; 2430334
 
 
 ;; TODO: Clean up above and use threading macros to improve readability
