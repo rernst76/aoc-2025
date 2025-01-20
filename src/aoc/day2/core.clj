9bc395502e2dbac946766f01f4e71bb23da561e9
Ryan Ernst <rernst@Ryans-Mac-mini.local>
2025-01-20 06:54:44 -0500

    Day2 complete, start day3

--- src/aoc/day2/core.clj
+++ src/aoc/day2/core.clj
@@ -47,8 +47,33 @@
   (and (safe-mag? xs)
        (safe-direction? xs)))
 
-(defn solve-p1 [path]
-  (map safe? (get-input path)))
+(defn solve-p1
+  "Map safe? onto input, filter just the true responses and count them."
+  [path]
+  (count (filter true? (map safe? (get-input path)))))
 
-(solve-p1 "resources/day2/day2sample.input")
+(defn with-dampened
+  "Returns the original seq, along with all potential dampened variations."
+  [xs]
+  (cons xs
+        (map-indexed
+         (fn [i _]
+           (keep-indexed
+            (fn [j x] (when (not= i j) x))
+            xs))
+         xs)))
+
+(defn safe-with-dampener?
+  "Returns true if the sequence is safe or can be made safe by removing one element."
+  [xs]
+  (some safe? (with-dampened xs)))
+
+(defn solve-p2 [path]
+  (count (filter true? (map safe-with-dampener? (get-input path)))))
+
+(solve-p1 "resources/day2/sample.input")
+(solve-p1 "resources/day2/input")
+
+(solve-p2 "resources/day2/sample.input")
+(solve-p2 "resources/day2/input")
 
